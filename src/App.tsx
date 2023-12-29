6d62a3a34f1ef1a03ab38f32dfee5d7cb7df4241
BearHumanS <bye0651@naver.com>
2023-12-30 01:02:39 +0900

    Feat: 공통 컴포넌트 및 공통 스타일 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import React from 'react'
-import logo from './logo.svg'
+import Text from '@common/Text'
+import Button from '@common/Button'
+import Input from '@common/Input'
+import TextField from '@common/TextField'
+import Alert from '@common/Alert'
+import { useAlertContext } from '@contexts/AlertContext'
 import './App.css'
 
 function App() {
+  const { open } = useAlertContext()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Text typography="t1" display="block" color="red">
+        t1
+      </Text>
+      <Text typography="t2">t1</Text>
+      <Text typography="t3">t1</Text>
+      <Text typography="t4">t1</Text>
+      <Text typography="t5">t1</Text>
+      <Text typography="t6">t1</Text>
+      <Text typography="t7">t1</Text>
+      <div style={{ height: 10, width: ' 100%', background: 'red' }}>
+        <Button color="success">클릭</Button>
+        <Button color="error">클릭</Button>
+        <Button>클릭</Button>
+        <Button color="success" weak={true}>
+          클릭
+        </Button>
+        <Button color="error" weak={true}>
+          클릭
+        </Button>
+        <Button weak={true}>클릭</Button>
+        <Button full disabled>
+          클릭
+        </Button>
+        <div style={{ height: 10, width: ' 100%', background: 'red' }}>
+          <Input placeholder="아이디" aria-invalid={false} />
+          <Input placeholder="아이디" aria-invalid={true} />
+          <TextField label="아이디" />
+          <TextField label="비밀번호" hasError />
+        </div>
+        <div style={{ height: 10, width: ' 100%', background: 'red' }}>
+          {/* <Alert title="dddd" open={true} onButtonClick={() => {}} /> */}
+          <Button
+            onClick={() => {
+              open({
+                title: '확인',
+                description: '확인확인',
+                onButtonClick: () => {},
+              })
+            }}
+          >
+            열기
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
