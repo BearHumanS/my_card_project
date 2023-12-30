45592aaa2c775c3a65622808d2649447cf2b328a
BearHumanS <bye0651@naver.com>
2023-12-30 19:11:00 +0900

    Feat: Scroll 최상에 위치 하게 ScrollToTop 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from '@pages/Home'
 import CardPage from './pages/Card'
+import ScrollToTop from './components/common/ScrollToTop'
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" Component={HomePage} />
         <Route path="/card/:id" Component={CardPage} />
