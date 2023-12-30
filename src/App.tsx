8f037bbf22f9e55d6e63fe8f116550adcc35fdf1
BearHumanS <bye0651@naver.com>
2023-12-30 18:26:58 +0900

    Feat: 카드 디테일 페이지 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from '@pages/Home'
-import TestPage from '@pages/Test'
+import CardPage from './pages/Card'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" Component={HomePage} />
-        <Route path="/test" Component={TestPage} />
+        <Route path="/card/:id" Component={CardPage} />
       </Routes>
     </BrowserRouter>
   )
