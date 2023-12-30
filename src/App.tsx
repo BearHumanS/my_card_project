b8e9bee6e34981514ea8d67ae652491adf03683f
BearHumanS <bye0651@naver.com>
2023-12-31 06:05:50 +0900

    Feat: 로그인 및 회원가입 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from '@pages/Home'
-import CardPage from './pages/Card'
-import ScrollToTop from './components/common/ScrollToTop'
+import CardPage from '@pages/Card'
+import ScrollToTop from '@common/ScrollToTop'
+import Navbar from '@common/Navbar'
+import SigninPage from '@pages/Signin'
+import SignupPage from '@pages/Signup'
 
 function App() {
   return (
     <BrowserRouter>
       <ScrollToTop />
+      <Navbar />
       <Routes>
         <Route path="/" Component={HomePage} />
         <Route path="/card/:id" Component={CardPage} />
+        <Route path="/signin" Component={SigninPage} />
+        <Route path="/signup" Component={SignupPage} />
       </Routes>
     </BrowserRouter>
   )
