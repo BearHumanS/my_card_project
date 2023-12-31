bfd83e15cdf7c3cb74c6ba5c5c1f55091b630f0d
BearHumanS <bye0651@naver.com>
2023-12-31 09:59:47 +0900

    Feat: user에 따른 조건부 렌더링 및 로그아웃 로직 추가

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,22 +1,43 @@
-import { colors } from '@/styles/color'
 import { css } from '@emotion/react'
 import { Link, useLocation } from 'react-router-dom'
+import useUser from '../hooks/auth/useUser'
+import { colors } from '@/styles/color'
 import Button from './Button'
 import Flex from './Flex'
+import { useCallback } from 'react'
+import { signOut } from 'firebase/auth'
+import { auth } from '@/remote/firebase'
 
 const Navbar = () => {
   const { pathname } = useLocation()
+  const user = useUser()
 
   const showButton = ['/signup', '/signin'].includes(pathname) === false
+
+  const handleLogout = useCallback(() => {
+    signOut(auth)
+  }, [])
+
+  const renderButton = useCallback(() => {
+    if (user !== null) {
+      return <Button onClick={handleLogout}>로그아웃</Button>
+    }
+
+    if (showButton) {
+      return (
+        <Link to="/signin">
+          <Button>로그인/회원가입</Button>
+        </Link>
+      )
+    }
+
+    return null
+  }, [handleLogout, showButton, user])
 
   return (
     <Flex justify="space-between" align="center" css={navbarContainerStyles}>
       <Link to="/">홈</Link>
-      {showButton ? (
-        <Link to="/signup">
-          <Button>로그인/회원가입</Button>
-        </Link>
-      ) : null}
+      {renderButton()}
     </Flex>
   )
 }
