31ac2952b1ec385badd29e8a8be05ed58d583b33
BearHumanS <bye0651@naver.com>
2024-01-01 03:16:19 +0900

    Feat: 카드 신청 시 사용자의 정보를 선택하는 컴포넌트 생성 및 로직 추가

--- src/components/apply/BasicInfo.tsx
+++ src/components/apply/BasicInfo.tsx
@@ -1,7 +1,77 @@
-import React from 'react'
+import { ChangeEvent, useCallback, useState } from 'react'
+import {
+  annualIncomeOptions,
+  creditScoreOptions,
+  paymentDateOptions,
+} from '@/constants/apply'
+import Select from '@common/Select'
+import { ApplyValues } from '@/types/apply'
+import FixedBottomButton from '../common/FixedBottomButton'
 
-const BasicInfo = () => {
-  return <div>BasicInfo</div>
+type InfoValues = Pick<
+  ApplyValues,
+  'annualIncome' | 'creditScore' | 'paymentDate'
+>
+
+const BasicInfo = ({
+  onNext,
+}: {
+  onNext: (infoValues: InfoValues) => void
+}) => {
+  const [infoValues, setInfoValues] = useState<InfoValues>({
+    annualIncome: '',
+    creditScore: '',
+    paymentDate: '',
+  })
+
+  const handleSelectChange = useCallback(
+    (e: ChangeEvent<HTMLSelectElement>) => {
+      setInfoValues((prev) => ({
+        ...prev,
+        [e.target.name]: e.target.value,
+      }))
+    },
+    [],
+  )
+
+  const allChecked = Object.values(infoValues).every((value) => value)
+
+  return (
+    <div>
+      <Select
+        label="연소득"
+        name="annualIncome"
+        options={annualIncomeOptions}
+        placeholder={annualIncomeOptions[0].label}
+        value={infoValues.annualIncome}
+        onChange={handleSelectChange}
+      />
+      <Select
+        label="신용점수"
+        name="creditScore"
+        options={creditScoreOptions}
+        placeholder={creditScoreOptions[0].label}
+        value={infoValues.creditScore}
+        onChange={handleSelectChange}
+      />
+      <Select
+        label="결제일"
+        name="paymentDate"
+        options={paymentDateOptions}
+        placeholder={paymentDateOptions[0].label}
+        value={infoValues.paymentDate}
+        onChange={handleSelectChange}
+      />
+
+      <FixedBottomButton
+        label="다음"
+        onClick={() => {
+          onNext(infoValues)
+        }}
+        disabled={allChecked === false}
+      />
+    </div>
+  )
 }
 
 export default BasicInfo
