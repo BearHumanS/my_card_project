b0e065b7410fe53f9f7f53b79b9f3332b16ecd20
BearHumanS <bye0651@naver.com>
2024-01-02 06:16:51 +0900

    Feat: 카드리스트 지연 로딩 및 스켈레톤 ui 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,9 @@
-import AddBanners from '@/components/home/AddBanners'
-import CardList from '@/components/home/CardList'
+import { Suspense } from 'react'
+import AddBanners from '@components/home/AdBanners'
+import CardList from '@components/home/CardList'
 import Top from '@common/Top'
+
+import ListRow from '@components/common/ListRow'
 
 const HomePage = () => {
   return (
@@ -10,7 +13,13 @@ const HomePage = () => {
         subTitle="회원님을 위해서 혜택이 좋은 카드를 모아봤습니다."
       />
       <AddBanners />
-      <CardList />
+      <Suspense
+        fallback={[...new Array(10)].map((_, index) => (
+          <ListRow.Skeleton key={index} />
+        ))}
+      >
+        <CardList />
+      </Suspense>
     </div>
   )
 }
