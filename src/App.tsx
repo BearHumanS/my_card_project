5c5a3bbdfbee0ea8db182df3175e61d6f20210e9
BearHumanS <bye0651@naver.com>
2024-01-02 06:17:37 +0900

    Feat: Layout 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from '@pages/Home'
 import CardPage from '@pages/Card'
 import ScrollToTop from '@common/ScrollToTop'
-import Navbar from '@common/Navbar'
 import SigninPage from '@pages/Signin'
 import SignupPage from '@pages/Signup'
 import Private from '@components/auth/Private'
@@ -11,43 +10,46 @@ import ApplyPage from '@pages/Apply'
 import ApplyDone from './pages/ApplyDone'
 import PageLoader from './components/common/PageLoader'
 import MyPage from './pages/My'
+import Layout from './components/Layout'
 
 function App() {
   return (
     <BrowserRouter>
       <ScrollToTop />
-      <Navbar />
+
       <Routes>
-        <Route path="/" Component={HomePage} />
-        <Route path="/card/:id" Component={CardPage} />
-        <Route path="/signin" Component={SigninPage} />
-        <Route path="/signup" Component={SignupPage} />
-        <Route
-          path="/apply/:id"
-          element={
-            <Private>
-              <Suspense fallback={<PageLoader />}>
-                <ApplyPage />
-              </Suspense>
-            </Private>
-          }
-        />
-        <Route
-          path="/apply/done"
-          element={
-            <Private>
-              <ApplyDone />
-            </Private>
-          }
-        />
-        <Route
-          path="/mypage"
-          element={
-            <Private>
-              <MyPage />
-            </Private>
-          }
-        />
+        <Route path="/" element={<Layout />}>
+          <Route path="/" Component={HomePage} />
+          <Route path="/card/:id" Component={CardPage} />
+          <Route path="/signin" Component={SigninPage} />
+          <Route path="/signup" Component={SignupPage} />
+          <Route
+            path="/apply/:id"
+            element={
+              <Private>
+                <Suspense fallback={<PageLoader />}>
+                  <ApplyPage />
+                </Suspense>
+              </Private>
+            }
+          />
+          <Route
+            path="/apply/done"
+            element={
+              <Private>
+                <ApplyDone />
+              </Private>
+            }
+          />
+          <Route
+            path="/mypage"
+            element={
+              <Private>
+                <MyPage />
+              </Private>
+            }
+          />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
