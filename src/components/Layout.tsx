e03fd31a9c51453f40a3e48e53413b7390736272
BearHumanS <bye0651@naver.com>
2024-01-02 06:18:00 +0900

    Feat: 라우터 변경을 Layout에서 관리

---
+++ src/components/Layout.tsx
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled'
+import { Outlet } from 'react-router-dom'
+import Navbar from './common/Navbar'
+
+function Layout() {
+  return (
+    <Container>
+      <Navbar />
+      <Outlet />
+    </Container>
+  )
+}
+const Container = styled.div`
+  height: 100%;
+  margin: 0 auto;
+  max-width: 480px;
+  overflow-y: auto;
+`
+
+export default Layout
