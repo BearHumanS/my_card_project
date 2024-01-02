3420af5d931985f28d0836fda1aa39950ba95398
BearHumanS <bye0651@naver.com>
2024-01-02 10:16:25 +0900

    Feat: comment mock데이터 추가

---
+++ src/constants/comment.ts
@@ -0,0 +1,18 @@
+export const USER_COMMENT = [
+  {
+    id: '고양이',
+    comment: '이 카드 너무 좋아요.',
+  },
+  {
+    id: '강아지',
+    comment: '혜택이 미쳤어요!',
+  },
+  {
+    id: '거북이',
+    comment: '이 카드만 사용할래요!',
+  },
+  {
+    id: '용',
+    comment: '다들 아직도 신청 안하셨어요?',
+  },
+]
